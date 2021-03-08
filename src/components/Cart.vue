49bc26d545f29a326a58419c40507414a1e90668
khatskelevich <khatskelevich@proitr.ru>
2021-03-08 19:54:16 +0500

    Create Cart component. Prototype of shop cart

---
+++ src/components/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cart">
+    <ul>
+      <li v-for="(item, key) in cart">
+        {{item.product.name}}, {{item.product.price}} * {{item.quantity}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "Cart",
+  computed: {
+    ...mapGetters('cart', {cart: 'getCartItems'})
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
