ff75730761c3f4d972c6d7d88f977ff78b9a8c1b
khatskelevich <khatskelevich@proitr.ru>
2021-03-08 19:53:19 +0500

    Create ProductCard component for product's details

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <Modal @close="$emit('close')">
+    <template v-slot:title>
+      <h3>Product</h3>
+    </template>
+    <template v-slot:body>
+      Name: {{ item.name }}.
+      Description: {{ item.description }}.
+      Price: {{ item.price }}
+    </template>
+  </Modal>
+
+</template>
+
+<script>
+import Modal from "@/components/Modal";
+
+export default {
+  name: "ProductCard",
+  components: {
+    Modal
+  },
+  props: {
+    item: Object
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
