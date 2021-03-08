e1525bfb2c71a2b50e1078aee0b4ae0a727a3e0d
khatskelevich <khatskelevich@proitr.ru>
2021-03-08 19:52:37 +0500

    Create Modal component for modal windows.

---
+++ src/components/Modal.vue
@@ -0,0 +1,110 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal-shadow" @click.self="closeModal">
+      <div class="modal">
+        <div class="modal-close" @click="closeModal">&#10006;</div>
+        <slot name="title" class="modal-title">
+          <!--            <h3 v-if="title">{{title}}</h3>-->
+        </slot>
+        <hr>
+        <slot name="body" class="modal-content">
+          <!--            <p v-if="body" v-html="body"></p>-->
+        </slot>
+        <hr>
+        <slot name="footer">
+          <div class="modal-footer">
+            <button class="modal-footer__button" @click="closeModal">
+              Close
+            </button>
+          </div>
+        </slot>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    show: false,
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    }
+  },
+  computed: {
+    // ...mapGetters({
+    //   title: 'getTitle',
+    //   body: 'getBody'
+    // })
+  }
+}
+</script>
+
+<style>
+.modal-shadow {
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.39);
+}
+
+.modal {
+  background: #fff;
+  border-radius: 8px;
+  padding: 15px;
+  min-width: 420px;
+  max-width: 480px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.modal-close {
+  border-radius: 50%;
+  color: #fff;
+  background: #2a4cc7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 7px;
+  right: 7px;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+}
+
+.modal-title {
+  color: #0971c7;
+}
+
+.modal-content {
+  margin-bottom: 20px
+}
+
+.modal-footer__button {
+  background-color: #0971c7;
+  color: #fff;
+  border: none;
+  text-align: center;
+  padding: 8px;
+  font-size: 17px;
+  font-weight: 500;
+  border-radius: 8px;
+  min-width: 150px;
+}
+
+.modal-enter-active, .modal-leave-active {
+  transition: opacity .3s ease;
+}
+
+.modal-enter, .modal-leave-to {
+  opacity: 0
+}
+</style>
